da276cbea93da98f0c0f4f7fa1c356888d0a3e7a
zyounger8 <101150212+zyounger8@users.noreply.github.com>
2024-03-03 14:47:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+*******************************
+* Programming Exercise        *
+* Computer Programming        *
+*    Author: Z. Y.            *
+*******************************
+  */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "CCCCCCCCC         ++               ++\n"  
+  <<"CC                ++               ++\n" 
+  <<"CC          ++++++++++++++   ++++++++++++++\n"
+  <<"CC          ++++++++++++++   ++++++++++++++\n"
+  <<"CC                ++               ++\n"
+  << "CCCCCCCCC         ++               ++\n"
+  <<endl;
+
+  
+  return 0;
+  
+}
